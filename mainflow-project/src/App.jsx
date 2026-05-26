47ffe1fbd5915072e9ed66058e0b3914a04da7da
Ashish <ashish233227shukla@gmail.com>
2026-05-26 21:40:24 +0530

    Added CSS File

---
+++ mainflow-project/src/App.jsx
@@ -0,0 +1,11 @@
+import StudentProfile from "./component/StudentProfile";
+export default function App()
+{
+  
+  return(
+    <>
+  <StudentProfile name ="Ashish" branch="MCA" college="KJU"/>
+  <StudentProfile name = "Amit" branch ="BCA" college="BHU"/>
+  </>
+  )
+}
