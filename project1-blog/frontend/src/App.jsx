c58f35d553405dc17b1f8d413f0810cc67b3761f
Ashish <ashish233227shukla@gmail.com>
2026-06-24 15:49:45 +0530

    updated App.jsx

--- project1-blog/frontend/src/App.jsx
+++ project1-blog/frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { Route,Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute"
+import CreatePost from "./pages/CreatePost";
 export default function App(){
   return(
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/create" element={<ProtectedRoute><CreatePost/></ProtectedRoute>}/>
     </Routes>
   )
 }
