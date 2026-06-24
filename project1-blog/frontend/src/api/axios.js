5b48417aacb0ab99637a358e56f001d67025437b
Ashish <ashish233227shukla@gmail.com>
2026-06-24 15:38:14 +0530

    added interceptor for config object with api

--- project1-blog/frontend/src/api/axios.js
+++ project1-blog/frontend/src/api/axios.js
@@ -1,4 +1,9 @@
 import axios from "axios"
 const API = axios.create({baseURL:"http://localhost:3000/api"})
+API.interceptors.request.use((config)=>{
+    const user = JSON.parse(localStorage.getItem("blogUser"));
+    if(user?.token) config.headers.Authorization = `Bearer ${user.token}`
+    return config;
+})
 
 export default API;
