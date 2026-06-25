f78c6223864c07567e9bde30080d72f25c3b684d
Ashish <ashish233227shukla@gmail.com>
2026-06-25 14:08:06 +0530

    updated for env

--- project1-blog/frontend/src/api/axios.js
+++ project1-blog/frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({baseURL:"http://localhost:3000/api"})
+const API = axios.create({baseURL:import.meta.env.VITE_API_URL || "http://localhost:3000/api"})
 API.interceptors.request.use((config)=>{
     const user = JSON.parse(localStorage.getItem("blogUser"));
     if(user?.token) config.headers.Authorization = `Bearer ${user.token}`
