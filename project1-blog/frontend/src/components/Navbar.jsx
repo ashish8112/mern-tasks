f5eaf99b697f784868342a34ccef40dc10def047
Ashish <ashish233227shukla@gmail.com>
2026-06-25 11:53:34 +0530

    added Home button for redirection ease

--- project1-blog/frontend/src/components/Navbar.jsx
+++ project1-blog/frontend/src/components/Navbar.jsx
@@ -7,7 +7,10 @@ export default function Navbar(){
     {
         return(
             <nav style={{margin:"10px 30px",display:"flex",justifyContent:"space-between"}}>
-            <img src="https://i.pinimg.com/736x/46/f7/95/46f79582fd13c8e87ad935f483899986.jpg" style={{height:"50px",width:"50px",borderRadius:"50%"}}></img>
+            <div>
+                <img onClick={()=>navigate("/")} src="https://images-workbench.99static.com/F5Mqto6RVGKC9hn49ZYk-ZrKfqE=/99designs-contests-attachments/72/72489/attachment_72489428" style={{height:"50px",width:"50px",borderRadius:"50%", cursor:"pointer"}}></img>
+            <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>navigate("/")}>Home</button>
+            </div>
             <div>
             <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>navigate("/create")}>Create Post</button>
             <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>logout()}>Logout</button>
@@ -18,7 +21,10 @@ export default function Navbar(){
     }
     return(
         <nav style={{margin:"10px 30px",display:"flex",justifyContent:"space-between"}}>
-            <img src="https://i.pinimg.com/736x/46/f7/95/46f79582fd13c8e87ad935f483899986.jpg" style={{height:"50px",width:"50px",borderRadius:"50%"}}></img>
+            <div>
+                <img onClick={()=>navigate("/")} src="https://images-workbench.99static.com/F5Mqto6RVGKC9hn49ZYk-ZrKfqE=/99designs-contests-attachments/72/72489/attachment_72489428" style={{height:"50px",width:"50px",borderRadius:"50%", cursor:"pointer"}}></img>
+            <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>navigate("/")}>Home</button>
+            </div>
             <div>
             <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>navigate("/login")}>Login</button>
             <button style={{padding:"10px 20px",cursor:"pointer"}} onClick={()=>navigate("/register")}>Register</button>
