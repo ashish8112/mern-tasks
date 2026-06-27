0eb0783ef6f22c28ccf56ddd485ebcb5570f3de3
Ashish <ashish233227shukla@gmail.com>
2026-06-27 07:07:48 +0530

    Comment of Working function

--- project1-blog/frontend/src/context/AuthContext.jsx
+++ project1-blog/frontend/src/context/AuthContext.jsx
@@ -5,24 +5,24 @@ const AuthContext = createContext();
 
 export default function AuthProvider({children}){
         const [user,setUser] = useState(
-            JSON.parse(localStorage.getItem("blogUser"))||null
+            JSON.parse(localStorage.getItem("blogUser"))||null // if user refresh the page set user with localStorage to avoid login everytime after refresh
         );
         async function register ({username,name,email,password}){
             const {data} = await API.post("/auth/register",{username,name,email,password})
-            return data;
+            return data; // good convention but we are not using it anywhere
         }
         async function login({email,password}){
             const {data} = await API.post("/auth/login",{email,password})
             localStorage.setItem("blogUser",JSON.stringify(data));
             setUser(data);
-            return(data);
+            return(data); // same as above 
         }
         async function logout (){
             localStorage.removeItem("blogUser");
             setUser(null);
         }
         return(
-            <AuthContext.Provider value= {{user,login,logout,register}}>
+            <AuthContext.Provider value= {{user,login,logout,register}}> // return these function to use across app
             {children}
             </AuthContext.Provider>
         )
